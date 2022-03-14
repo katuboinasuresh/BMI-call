8cbe1c9e2d19bd78e49ab22565ebd063b5e57711
katuboinasuresh <katuboinasuresh@gmail.com>
2022-03-14 21:41:04 +0530

    update_todolist

--- assignments/todo_list/script.js
+++ assignments/todo_list/script.js
@@ -1,4 +1,4 @@
-let inputtext = document.querySelector('input');
+/*let inputtext = document.querySelector('input');
 let addtodo = document.querySelector('button');
 let todolist = document.querySelector('div.todos');
 let count = 0;
@@ -16,4 +16,46 @@ addtodo.onclick = function(){
     })
     count++;
     }
+}*/
+let compBtn = document.querySelector("#complete");
+let pendBtn = document.querySelector("#pending");
+let alldata = document.querySelector("#user");
+let todolist = document.querySelector(".todos");
+
+fetechtodo();
+
+async function fetechtodo(){
+    const res=await fetch("https://jsonplaceholder.typicode.com/todos");
+    const data=await res.json();
+
+    alldata.addEventListener("click", () => {
+        getdata(data);
+    });
+    compBtn.addEventListener("click", () => {
+        let truedatalist = data.filter(ele => ele.completed);
+        getdata(truedatalist);
+    });
+    pendBtn.addEventListener("click", () => {
+        let falsedatalist = data.filter(ele => !ele.completed);
+        getdata(falsedatalist);
+    });
+
+}
+function getdata(data){
+    let output="";
+    data.forEach(todo => {
+        let check;
+        if(todo.completed){
+            check = "completed"
+        }
+        else{
+            check = "pending"
+        }
+        output+=`<div class="value_d">
+                <h4>${todo.title}</h4>
+                <p>userId : ${todo.userId}</p>
+                <p> ID : ${todo.id}</p>
+                <p>status :${check}</p>
+                </div>`
+    });todolist.innerHTML=output;
 }
